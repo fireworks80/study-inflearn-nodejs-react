be464e37f9f0fd4e87dc57e5d3cc54bedc246ce0
hanseungchul <schan@osci.kr>
2020-11-26 16:10:00 +0900

    logout 완료

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,11 @@
-const { User } = require('../models/User');
+const User = require('../models/User');
 
 const auth = (req, res, next) => { 
   // 인증 처리 하는곳
 
   // client에서 토큰 가져온다
-  const token = req.cookie.x_auth;
-
+  const token = req.cookies.x_auth;
+  
   // 토큰을 복호화 한 후 유저를 찾는다.
   User.findByToken(token, (err, user) => { 
     if (err) throw err;
